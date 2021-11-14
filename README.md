a667a73a90adc071ed894a2367e790ba252a4ad4
Carlos Sanchez <randomouscrap98@aol.com>
2021-11-14 23:24:56 +0000

    I just do not understand warp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # krustland
 Implementation and tools for running/migrating old kland from sbs
+
+## Install
+As a general rust... thing, you need several packages:
+
+### Ubuntu:
+`sudo apt install libssl-dev libsqlite3-dev pkg-config`
--- kland/src/main.rs
+++ kland/src/main.rs
@@ -1,33 +1,53 @@
 use warp::Filter;
 use std::error::Error;
 use serde::Deserialize;
-use s3::{ bucket::Bucket, credentials::Credentials, region::Region }
+use s3::{ bucket::Bucket, creds::Credentials, region::Region }; //, S3Error };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
    let settings = get_settings()?;
+   let bucket = make_bucket(&settings)?;
+   let rbucket = &bucket;
 
-    let s3image = warp::path!("i" / String)
-        .map(|imagename| {
-            format!("You asked for: {}", imagename)
-        });
-    
-    let addr = if settings.hostglobal { [0,0,0,0] } else { [127,0,0,1] };
+   let s3image = warp::path!("i" / String)
+       .and_then(|imagename| async move {
+           let bucketresult = rbucket.get_object(&imagename).await;
+           match bucketresult {
+               Ok((data, code)) => {
+                   Ok(format!("You asked for: {}, was {} bytes", imagename, data.len()))
+               },
+               Err(e) => Err(warp::reject::not_found())
+           }
+       });
 
-    println!("Running on {:?}:{}", addr, settings.port);
+   let routes = s3image.with(warp::cors().allow_any_origin());
 
-    warp::serve(s3image)
-        .run((addr, settings.port))
-        .await;
-    
-    Ok(())
+   let addr = if settings.hostglobal { [0,0,0,0] } else { [127,0,0,1] };
+
+   println!("Running on {:?}:{}", addr, settings.port);
+
+   warp::serve(routes)
+       .run((addr, settings.port))
+       .await;
+
+   Ok(())
 }
+
+//fn with_imagedata(bucket: &Bucket, name: String) -> impl Filter<Extract = (Vec<u8>,), Error = Infallible> + Clone {
+//    warp::any().map(move || {
+//
+//    })
+//}
 
 #[derive(Debug, Deserialize)]
 pub struct MySettings {
     pub klanddb : String, 
     pub klandstatic : String,
     pub hostglobal : bool,
+    pub s3accesskey : String,
+    pub s3secretkey : String,
+    pub s3bucket : String,
+    pub s3region : String,
     pub port : u16 
 }
 
@@ -39,3 +59,11 @@ fn get_settings() -> Result<MySettings, config::ConfigError> {
    Ok(myset) //transfer ownership
 }
 
+// Retrieve a configured bucket for connecting to kland images
+fn make_bucket(settings: &MySettings) -> Result<Bucket, Box<dyn Error>> {
+    let credentials: Credentials = Credentials::from_env_specific(
+        Some(&settings.s3accesskey), Some(&settings.s3secretkey), None, None)?;
+    let region: Region = settings.s3region.parse()?;
+    let bucket: Bucket = Bucket::new(&settings.s3bucket, region, credentials)?; 
+    Ok(bucket)
+}
